3d6430d356f649750461deb20440f7ca9e6f6743
kubeeverner-PF <kubee.verner@gmail.com>
2026-03-23 16:12:01 +0100

    Update PluginEditor.h

--- source/PluginEditor.h
+++ source/PluginEditor.h
@@ -1,25 +1,18 @@
 #pragma once
-
+#include <JuceHeader.h>
 #include "PluginProcessor.h"
-#include "BinaryData.h"
-#include "melatonin_inspector/melatonin_inspector.h"
 
-//==============================================================================
-class PluginEditor : public juce::AudioProcessorEditor
+class StepSequencerAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
 public:
-    explicit PluginEditor (PluginProcessor&);
-    ~PluginEditor() override;
-
-    //==============================================================================
+    StepSequencerAudioProcessorEditor (StepSequencerAudioProcessor&);
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void mouseDown(const juce::MouseEvent&) override;
+
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    PluginProcessor& processorRef;
-    std::unique_ptr<melatonin::Inspector> inspector;
-    juce::TextButton inspectButton { "Inspect the UI" };
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginEditor)
+    StepSequencerAudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (StepSequencerAudioProcessorEditor)
 };
