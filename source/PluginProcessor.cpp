51a984e029537df1d8aada10e2341f1dc06cc8e8
kubeeverner-PF <kubee.verner@gmail.com>
2026-03-23 16:11:42 +0100

    Update PluginProcessor.cpp

--- source/PluginProcessor.cpp
+++ source/PluginProcessor.cpp
@@ -1,186 +1,93 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-//==============================================================================
-PluginProcessor::PluginProcessor()
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+StepSequencerAudioProcessor::StepSequencerAudioProcessor() {}
+StepSequencerAudioProcessor::~StepSequencerAudioProcessor() {}
+
+void StepSequencerAudioProcessor::prepareToPlay (double sr, int)
 {
+    sampleRate = sr;
 }
 
-PluginProcessor::~PluginProcessor()
+void StepSequencerAudioProcessor::sendLaunchpadInit(juce::MidiBuffer& midi)
 {
+    // Programmer mode ON
+    uint8 sysex[] = { 0xF0,0x00,0x20,0x29,0x02,0x18,0x0E,0x01,0xF7 };
+    midi.addEvent(juce::MidiMessage(sysex, sizeof(sysex)), 0);
 }
 
-//==============================================================================
-const juce::String PluginProcessor::getName() const
+void StepSequencerAudioProcessor::processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer& midi)
 {
-    return JucePlugin_Name;
-}
+    midi.clear();
 
-bool PluginProcessor::acceptsMidi() const
-{
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-bool PluginProcessor::producesMidi() const
-{
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-bool PluginProcessor::isMidiEffect() const
-{
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-double PluginProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
-}
-
-int PluginProcessor::getNumPrograms()
-{
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
-}
-
-int PluginProcessor::getCurrentProgram()
-{
-    return 0;
-}
-
-void PluginProcessor::setCurrentProgram (int index)
-{
-    juce::ignoreUnused (index);
-}
-
-const juce::String PluginProcessor::getProgramName (int index)
-{
-    juce::ignoreUnused (index);
-    return {};
-}
-
-void PluginProcessor::changeProgramName (int index, const juce::String& newName)
-{
-    juce::ignoreUnused (index, newName);
-}
-
-//==============================================================================
-void PluginProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
-{
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
-    juce::ignoreUnused (sampleRate, samplesPerBlock);
-}
-
-void PluginProcessor::releaseResources()
-{
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
-}
-
-bool PluginProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
-
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
-
-    return true;
-  #endif
-}
-
-void PluginProcessor::processBlock (juce::AudioBuffer<float>& buffer,
-                                              juce::MidiBuffer& midiMessages)
-{
-    juce::ignoreUnused (midiMessages);
-
-    juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    // DAW sync
+    if (auto* playHead = getPlayHead())
     {
-        auto* channelData = buffer.getWritePointer (channel);
-        juce::ignoreUnused (channelData);
-        // ..do something to the data...
+        juce::AudioPlayHead::CurrentPositionInfo pos;
+        if (playHead->getCurrentPosition(pos))
+        {
+            if (pos.isPlaying)
+            {
+                double ppq = pos.ppqPosition;
+                int step = (int)(ppq * 4.0) % 64;
+
+                if (step != currentStep)
+                {
+                    currentStep = step;
+                    advance(midi);
+                }
+            }
+        }
     }
 }
 
-//==============================================================================
-bool PluginProcessor::hasEditor() const
+void StepSequencerAudioProcessor::advance(juce::MidiBuffer& midi)
 {
-    return true; // (change this to false if you choose to not supply an editor)
+    for (int i = 0; i < 64; i++)
+    {
+        if (i == currentStep)
+            sendLED(i, 0, 63, 0, midi); // green
+        else if (steps[i])
+            sendLED(i, 63, 0, 0, midi); // red
+        else
+            sendLED(i, 0, 0, 0, midi);
+
+        if (i == currentStep && steps[i])
+            sendDrum(i, midi);
+    }
 }
 
-juce::AudioProcessorEditor* PluginProcessor::createEditor()
+void StepSequencerAudioProcessor::sendLED(int step, uint8 r, uint8 g, uint8 b, juce::MidiBuffer& midi)
 {
-    return new PluginEditor (*this);
+    int note = (step / 8) * 16 + (step % 8);
+
+    uint8 msg[] = {
+        0xF0,0x00,0x20,0x29,0x02,0x18,0x0B,
+        (uint8)note,
+        r, g, b,
+        0xF7
+    };
+
+    midi.addEvent(juce::MidiMessage(msg, sizeof(msg)), 0);
 }
 
-//==============================================================================
-void PluginProcessor::getStateInformation (juce::MemoryBlock& destData)
+void StepSequencerAudioProcessor::sendDrum(int step, juce::MidiBuffer& midi)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
-    juce::ignoreUnused (destData);
+    int row = step / 8;
+
+    int note = 36; // kick default
+
+    if (row == 1) note = 38; // snare
+    if (row == 2) note = 42; // hihat
+
+    midi.addEvent(juce::MidiMessage::noteOn(1, note, (uint8)100), 0);
+    midi.addEvent(juce::MidiMessage::noteOff(1, note), 120);
 }
 
-void PluginProcessor::setStateInformation (const void* data, int sizeInBytes)
-{
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
-    juce::ignoreUnused (data, sizeInBytes);
-}
+void StepSequencerAudioProcessor::setStep(int i, bool s) { steps[i] = s; }
+bool StepSequencerAudioProcessor::getStep(int i) const { return steps[i]; }
 
-//==============================================================================
-// This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+juce::AudioProcessorEditor* StepSequencerAudioProcessor::createEditor()
 {
-    return new PluginProcessor();
+    return new StepSequencerAudioProcessorEditor (*this);
 }
