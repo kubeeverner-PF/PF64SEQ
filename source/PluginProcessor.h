b2ac1e69625ce7d5a92b60a21c4451768ae98e9f
kubeeverner-PF <kubee.verner@gmail.com>
2026-03-23 16:11:21 +0100

    Update PluginProcessor.h

--- source/PluginProcessor.h
+++ source/PluginProcessor.h
@@ -1,43 +1,38 @@
 #pragma once
+#include <JuceHeader.h>
 
-#include <juce_audio_processors/juce_audio_processors.h>
-
-#if (MSVC)
-#include "ipps.h"
-#endif
-
-class PluginProcessor : public juce::AudioProcessor
+class StepSequencerAudioProcessor  : public juce::AudioProcessor
 {
 public:
-    PluginProcessor();
-    ~PluginProcessor() override;
+    StepSequencerAudioProcessor();
+    ~StepSequencerAudioProcessor() override;
 
-    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
-    void releaseResources() override;
-
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
-
+    void prepareToPlay (double, int) override;
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
 
     juce::AudioProcessorEditor* createEditor() override;
-    bool hasEditor() const override;
+    bool hasEditor() const override { return true; }
 
-    const juce::String getName() const override;
+    bool acceptsMidi() const override { return true; }
+    bool producesMidi() const override { return true; }
+    bool isMidiEffect() const override { return true; }
 
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
-
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const juce::String getProgramName (int index) override;
-    void changeProgramName (int index, const juce::String& newName) override;
-
-    void getStateInformation (juce::MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
+    void sendLaunchpadInit(juce::MidiBuffer&);
+    void setStep(int index, bool state);
+    bool getStep(int index) const;
 
 private:
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginProcessor)
+    bool steps[64] = { false };
+    int currentStep = 0;
+
+    double bpm = 120.0;
+    double sampleRate = 44100.0;
+    int samplesPerStep = 0;
+    int counter = 0;
+
+    void advance(juce::MidiBuffer&);
+    void sendLED(int step, juce::uint8 r, juce::uint8 g, juce::uint8 b, juce::MidiBuffer&);
+    void sendDrum(int step, juce::MidiBuffer&);
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (StepSequencerAudioProcessor)
 };
